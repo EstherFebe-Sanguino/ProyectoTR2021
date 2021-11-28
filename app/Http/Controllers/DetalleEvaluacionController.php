5837d9c990939d4da0c399e6dcab5ff0c8dee234
EstherFebe-Sanguino <estherfebesanguino@gmail.com>
2021-11-28 15:58:53 -0400

    creacion de modelos, migraciones y controladores del proyecto

---
+++ app/Http/Controllers/DetalleEvaluacionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DetalleEvaluacion;
+use Illuminate\Http\Request;
+
+class DetalleEvaluacionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\DetalleEvaluacion  $detalleEvaluacion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DetalleEvaluacion $detalleEvaluacion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\DetalleEvaluacion  $detalleEvaluacion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DetalleEvaluacion $detalleEvaluacion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\DetalleEvaluacion  $detalleEvaluacion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, DetalleEvaluacion $detalleEvaluacion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\DetalleEvaluacion  $detalleEvaluacion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DetalleEvaluacion $detalleEvaluacion)
+    {
+        //
+    }
+}
