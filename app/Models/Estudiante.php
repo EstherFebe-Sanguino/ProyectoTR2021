4053bf143d52c88524d895c4a7d74997e01d5c25
EstherFebe-Sanguino <89990271+EstherFebe-Sanguino@users.noreply.github.com>
2021-12-10 12:07:42 -0400

    Update Estudiante.php

--- app/Models/Estudiante.php
+++ app/Models/Estudiante.php
@@ -1,11 +1,14 @@
 <?php
 
 namespace App\Models;
-
+use App\Model\Detalle_evaluacion;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Estudiante extends Model
 {
     use HasFactory;
+    public function detalle_evaluacion(){
+        return $this->hasMany(Detalle_evaluacion::class);} 
+
 }
