8e6c5fa087be5eddd5d6d655849ddd1d8eb38f96
EstherFebe-Sanguino <89990271+EstherFebe-Sanguino@users.noreply.github.com>
2021-12-10 12:05:59 -0400

    Update DetalleEvaluacion.php

--- app/Models/DetalleEvaluacion.php
+++ app/Models/DetalleEvaluacion.php
@@ -1,11 +1,13 @@
 <?php
 
 namespace App\Models;
-
+use App\Model\Estudiante;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class DetalleEvaluacion extends Model
 {
     use HasFactory;
+    public function estudiante(){
+        return $this->belongsTo(Estudiante::class);}
 }
