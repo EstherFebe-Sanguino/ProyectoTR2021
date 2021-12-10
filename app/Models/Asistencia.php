d1451f0dd8757efc8aa34d9ad4cfb88034fd21a4
EstherFebe-Sanguino <89990271+EstherFebe-Sanguino@users.noreply.github.com>
2021-12-10 11:06:02 -0400

    Update Asistencia.php

--- app/Models/Asistencia.php
+++ app/Models/Asistencia.php
@@ -1,11 +1,13 @@
 <?php
 
 namespace App\Models;
-
+use App\Model\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Asistencia extends Model
 {
     use HasFactory;
+        public function users(){
+        return $this->belongsTo(users::class);}
 }
