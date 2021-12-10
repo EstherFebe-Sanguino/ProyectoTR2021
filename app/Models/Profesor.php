ff64872807489c566986cae2f0c389490e5ec3a7
EstherFebe-Sanguino <89990271+EstherFebe-Sanguino@users.noreply.github.com>
2021-12-10 12:18:04 -0400

    Update Profesor.php

--- app/Models/Profesor.php
+++ app/Models/Profesor.php
@@ -1,11 +1,13 @@
 <?php
 
 namespace App\Models;
-
+use App\Model\Evaluacion;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Profesor extends Model
 {
     use HasFactory;
+    public function evaluacion(){
+        return $this->hasMany(Evaluacion::class);}
 }
