62d271c95f8c89d106af017f97dd206534895b1a
EstherFebe-Sanguino <89990271+EstherFebe-Sanguino@users.noreply.github.com>
2021-12-10 11:42:01 -0400

    Update Materia.php

--- app/Models/Materia.php
+++ app/Models/Materia.php
@@ -1,11 +1,13 @@
 <?php
 
 namespace App\Models;
-
+use App\Model\Activides;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Materia extends Model
 {
     use HasFactory;
+    public function actividades(){
+        return $this->hasMany(Actividades::class);} 
 }
