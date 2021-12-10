28d95fe2bd02810b43885bbfa49bb4b08731da23
EstherFebe-Sanguino <89990271+EstherFebe-Sanguino@users.noreply.github.com>
2021-12-10 11:39:12 -0400

    Update Actividades.php

--- app/Models/Actividades.php
+++ app/Models/Actividades.php
@@ -1,11 +1,14 @@
 <?php
 
 namespace App\Models;
-
+use App\Model\Materia;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Actividades extends Model
 {
     use HasFactory;
+    
+public function materia(){
+        return $this->belongsTo(Materia::class);}    
 }
