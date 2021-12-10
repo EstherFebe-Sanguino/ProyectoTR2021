1d09d76c1fd939160b325d26217cb8ba64882c99
EstherFebe-Sanguino <89990271+EstherFebe-Sanguino@users.noreply.github.com>
2021-12-10 11:44:02 -0400

    Update Tema.php

--- app/Models/Tema.php
+++ app/Models/Tema.php
@@ -1,11 +1,13 @@
 <?php
 
 namespace App\Models;
-
+use App\Model\Materia;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Tema extends Model
 {
-    use HasFactory;
+    use HasFactory
+        public function materia(){
+        return $this->belongsTo(Materia::class);
 }
