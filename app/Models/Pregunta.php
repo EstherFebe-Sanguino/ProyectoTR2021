be140f7c5b35f4dce871e83b06ee95f591f7d0c1
EstherFebe-Sanguino <89990271+EstherFebe-Sanguino@users.noreply.github.com>
2021-12-10 12:01:46 -0400

    Update Pregunta.php

--- app/Models/Pregunta.php
+++ app/Models/Pregunta.php
@@ -1,11 +1,13 @@
 <?php
 
 namespace App\Models;
-
+use App\Model\Tema;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Pregunta extends Model
 {
     use HasFactory;
+        public function tema(){
+        return $this->belongsTo(Tema::class);}
 }
