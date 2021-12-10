57146f6a84fc30a8bd3fda6ea6073f5b24ad52af
EstherFebe-Sanguino <89990271+EstherFebe-Sanguino@users.noreply.github.com>
2021-12-10 12:14:38 -0400

    Update Evaluacion.php

--- app/Models/Evaluacion.php
+++ app/Models/Evaluacion.php
@@ -1,11 +1,22 @@
 <?php
 
 namespace App\Models;
-
+use App\Model\Estudiante;
+use App\Model\Tema;
+use App\Model\Profesor;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Evaluacion extends Model
 {
     use HasFactory;
+    
+public function estudiante(){
+        return $this->belongsTo(Estudiante::class);}
+    
+    public function tema(){
+        return $this->belongsTo(Tema::class);}
+
+public function profesor(){
+        return $this->belongsTo(Profesor::class);}
 }
